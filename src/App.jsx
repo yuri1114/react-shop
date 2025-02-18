bd2533a67e721c6b26d7a9b1f62b3fd86c2124b7
장유리 <jyuri1114@naver.com>
2025-02-18 13:21:10 +0900

    [Add] 메인페이지 카드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
-import Button from "react-bootstrap/Button";
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import data from "./data.js";
 import "./App.css";
 
 function App() {
+  let [product] = useState(data);
+
   return (
     <div className="App">
-      {/* <Button variant="dark">Dark</Button> */}
-
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Fabric Poster</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">Home </Nav.Link>
             <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <div className="main-bg"></div>
+
+      <div className="product-content">
+        {product.map(function (a, i) {
+          return (
+            <ItemCard
+              product={product[i]}
+              i={i}
+            ></ItemCard>
+          );
+        })}
+      </div>
     </div>
   );
+
+  function ItemCard(props) {
+    return (
+      <div className="item-card">
+        <div className="card">
+          <img src={"/product0" + props.i + ".jpg"} />
+        </div>
+        <div className="text-wrap">
+          <p className="product-title">{props.product.title}</p>
+          <p className="product-des">{props.product.content}</p>
+          <p className="product-des">{props.product.price}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+let data = [
+  {
+    id: 0,
+    title: "midsummer sunshine",
+    content: "Born in France",
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: "forest lake",
+    content: "Born in Seoul",
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: "Good morning, New York",
+    content: "Born in the States",
+    price: 130000,
+  },
+];
+export default data;
